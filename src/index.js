d7f79dfbd4e403925983c956efb39f7111c3d433
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-06-02 00:07:44 +0530

    App deployed initially

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-import App from './App';
 
 root.render(
     <App />
