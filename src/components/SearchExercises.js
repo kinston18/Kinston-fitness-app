e5a853bd5eb265f9396f17cc3f1c5c17117870e0
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-06-04 00:42:32 +0530

    Hero banner and Search bar done

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, {useEffect,useState} from 'react';
+import {Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 const SearchExercises = () => {
   return (
-    <div>
-      SearchExercises
-    </div>
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography fontWeight={700} sx={{fontSize: {lg:'44px', xs:'30px'}}}
+      mb="50px" textAlign="center">
+        Exercises <br/>You Must Know 
+      </Typography>
+
+      <Box position="relative" mb="72px" >
+        <TextField 
+        sx={{
+          input:{ fontWeight:'700', 
+          border:'none',
+          borderRadius: '4px'},
+          width: {lg: '1170px', xs:'350px'},
+          backgroundColor: '#ffff',
+          borderRadius:'40px'
+        }}
+        height="76px"
+        value=""
+        onChange={(e) => {}}
+        placeholder='Search Exercises'
+        type='text'
+        />
+      </Box>
+    </Stack>
   )
 }
 
