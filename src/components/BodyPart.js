e0a6133e94f2af38bfb0ae56623e705aa61dcc34
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-06-04 23:27:54 +0530

    cards in making

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -5,7 +5,19 @@ import Icon from '../assets/icons/gym.png';
 
 const BodyPart = () => {
 return (
-    <Stack>
+    <Stack
+    type = "button"
+    alignItems = "center"
+    justifyContent= "center"
+    className = "bodyPart-card"
+
+    sx={
+        bodyPart === item ? {
+                borderTop : '4px solid #ff2625',
+                backgroundColor : '#ffff'
+        } : 
+    } 
+    >
         <img src={Icon} alt='dumbbell' styles ={ { width :'40px', height:'40px'} }/>
     </Stack>
   )
