1a221dacc975769e48d0d4ee06da7838e6afab7c
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-06-05 23:41:25 +0530

    exercise card added

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -3,6 +3,7 @@ import { Pagination } from '@mui/material/Pagination';
 import { Box, Stack, Typography } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({ exercises,setExercises,bodyParts}) => {
     console.log(exercises);
@@ -12,13 +13,13 @@ const Exercises = ({ exercises,setExercises,bodyParts}) => {
     mt="50px"
     p="20px">
 
-      <Typography variant='h3' marginBottom="46px">
+      <Typography variant='h3' marginBottom="46px" fontWeight="550">
         Showing Results
       </Typography>
       <Stack direction="row" sx={{gap: { lg:'110px', xs:'50px'}}}
       flexWrap="wrap" justifyContent="center">
         {exercises.map((exercise,index) => (
-          <p>exercise.name</p>
+          <ExerciseCard  key={index} exercise={exercise} />
         )
         )}
 
