4ad3e3e64754a247f0ce92d45e25dff7ecfc9542
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-06-06 01:15:33 +0530

    search complete with pagination

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,12 +1,24 @@
 import React, { useEffect,useState} from 'react';
-import { Pagination } from '@mui/material/Pagination';
+import Pagination  from '@mui/material/Pagination';
 import { Box, Stack, Typography } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
 import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({ exercises,setExercises,bodyParts}) => {
-    console.log(exercises);
+  const [currentPage, setCurrentPage] = useState(1); 
+  const exercisesPerPage = 10;
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice(indexOfFirstExercise,indexOfLastExercise);
+
+  const paginate = (e,value) => {
+    setCurrentPage(value);
+
+    window.scrollTo({top : 1800, behavior: 'smooth'})
+
+  }
+
   return (
    <Box id="exercises"
    sx={{mt: {lg:'110px'}}}
@@ -18,9 +30,23 @@ const Exercises = ({ exercises,setExercises,bodyParts}) => {
       </Typography>
       <Stack direction="row" sx={{gap: { lg:'110px', xs:'50px'}}}
       flexWrap="wrap" justifyContent="center">
-        {exercises.map((exercise,index) => (
+        {currentExercises.map((exercise,index) => (
           <ExerciseCard  key={index} exercise={exercise} />
         )
+        )}
+
+      </Stack>
+      <Stack mt="100px" alignItems="center" >
+        {exercises.length > 9 && (
+          <Pagination
+          color="standard" 
+          shape="rounded"
+          defaultPage={1}
+          count = {Math.ceil(exercises.length / exercisesPerPage)}
+          page={currentPage}
+          onChange= {paginate}
+          size="large"
+          />
         )}
 
       </Stack>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,8 +18,8 @@ const Navbar = () => {
       fontSize="24px"
       alignItems={"flex-end"}
       >
-        <Link to="/" style={{ textDecoration:'none', color:"#3A1212", borderBottom:'3px solid #FF2625' }}>Home</Link>
-        <a href='#exercises' style={{textDecoration:'none', color:'#3A1212'}}>Exercises</a>
+        <Link to="/" className="hover-link" style={{ textDecoration:'none', color:"#3A1212" /*, borderBottom:'3px solid #FF2625' */ }}>Home</Link>
+        <a className="hover-link" href='#exercises' style={{textDecoration:'none', color:'#3A1212'}}>Exercises</a>
       </Stack>
     </Stack>
   )
